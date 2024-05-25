32bfe7afad559a576a5f321251ff5c35ab734c30
Miriã Bastos <142639375+MiriaBastos@users.noreply.github.com>
2024-05-25 12:25:01 -0300

    Adiciona css ao componente de erro

--- system/resources/js/Components/InputError.vue
+++ system/resources/js/Components/InputError.vue
@@ -8,7 +8,7 @@ defineProps({
 
 <template>
     <div v-show="message">
-        <p class="text-sm text-red-600">
+        <p class="text-sm alert alert-danger text-sm">
             {{ message }}
         </p>
     </div>

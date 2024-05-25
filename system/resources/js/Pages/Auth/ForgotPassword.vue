5d387b8b4ebb985878a635d4176217d03426fca5
Miriã Bastos <142639375+MiriaBastos@users.noreply.github.com>
2024-05-25 12:25:43 -0300

    Ajusta legendas do forgotPassoword

--- system/resources/js/Pages/Auth/ForgotPassword.vue
+++ system/resources/js/Pages/Auth/ForgotPassword.vue
@@ -25,37 +25,37 @@ const submit = () => {
     <GuestLayout>
         <Head title="Forgot Password" />
 
-        <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset
-            link that will allow you to choose a new one.
+        <div class="mb-3">
+            Esqueceu sua senha? Sem problemas. Basta nos informar seu endereço de e-mail
+            e enviaremos por e-mail um link de redefinição de senha que permitirá que
+            você escolha uma nova
         </div>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+        <div v-if="status" class="mb-3">
             {{ status }}
         </div>
 
         <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+                <div class="input-group">
+                    <TextInput
+                        id="email"
+                        type="email"
+                        v-model="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                    />
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </PrimaryButton>
-            </div>
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 mt-3">
+                        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Link de redefinição de senha de e-mail
+                        </PrimaryButton>
+                    </div>
+                </div>
         </form>
     </GuestLayout>
 </template>

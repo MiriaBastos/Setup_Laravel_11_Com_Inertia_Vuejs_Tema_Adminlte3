b7a7b1abcc52ffd5bfb69a774ddb65e8ea1caf1a
Miriã Bastos <142639375+MiriaBastos@users.noreply.github.com>
2024-07-20 17:17:50 -0300

    Ajusta listagem para receber de forma dinamica o cadastro.

--- system/resources/js/Pages/Master/Listagem/index.vue
+++ system/resources/js/Pages/Master/Listagem/index.vue
@@ -3,45 +3,49 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import TextInput from '@/Components/TextInput.vue';
 import Modal from '@/Components/Modal.vue';
-
-
-import { Link, useForm } from '@inertiajs/vue3';
+import { Head, Link, useForm, usePage } from '@inertiajs/vue3';
 import { nextTick, ref } from 'vue';
 
-const confirmingFormAdd = ref(false);
+const exibirModal = ref(false);
 const nomeInput = ref(null);
 
 const form = useForm({
     nome: '',
 });
 
-
-const confirmFormAdd = () => {
-    confirmingFormAdd.value = true;
-
+const abrirModal = () => {
+    exibirModal.value = true;
     nextTick(() => nomeInput.value.focus());
 };
 
-// acao para adicionar um novo registro
-const cadastraLista = () => {
-    form.cadastra(route('profile.destroyl'), {
+const cadastrarLista = () => {
+    form.post(route('listagem.cadastrar'), {
         preserveScroll: true,
-        onSuccess: () => closeModal(),
+        onSuccess: (page) => {
+            const newItem = page.props.lista;
+            if (newItem) {
+                itens.value.push(newItem);
+            }
+            closeModal();
+        },
         onError: () => nomeInput.value.focus(),
         onFinish: () => form.reset(),
     });
 };
 
 const closeModal = () => {
-    confirmingFormAdd.value = false;
-
+    exibirModal.value = false;
     form.reset();
 };
+
+// Obtenha os props da página atual
+const { props } = usePage();
+const lista = ref(props.lista);
+const itens = ref(props.listaCompras);
 
 </script>
 
 <template>
-
     <Head title="Listagem" />
 
     <AuthenticatedLayout>
@@ -50,89 +54,52 @@ const closeModal = () => {
         </template>
 
         <div class="col-sm-12">
+            <button @click="abrirModal" class="btn btn-info snRegular btn-block" style="border-radius: 20px;">
+                CRIE UMA NOVA LISTAGEM
+            </button>
+        </div>
+
+        <div v-if="itens.length" class="col-sm-12">
             <br>
             <table class="table" border="0">
-
                 <tbody>
-                    <tr class="bg-success">
-                        <td style="border-radius: 20px; border: none;">
-                            <i class="fa fa-list"></i>&nbsp;
-                            <a href="#" class="link snRegular text-white">
-                                <strong>LISTAGEM DO MÊS</strong>
-                            </a>
-                        </td>
-                    </tr>
-                    <br>
-                    <tr class="bg-success">
-                        <td style="border-radius: 20px; border: none;">
-                            <i class="fa fa-list"></i>&nbsp;
-                            <strong>LISTAGEM DA SEMANA</strong>
-                        </td>
-                    </tr>
-                    <br>
-                    <tr class="bg-success">
-                        <td style="border-radius: 20px; border: none;">
-                            <i class="fa fa-list"></i>&nbsp;
-                            <strong>LISTAGEM DE ITENS EM GERAL</strong>
-                        </td>
+                    <tr v-for="item in itens" :key="item.id">
+                        <Link :href="route('listagem.index', { lista_id: item.id })" class="link snRegular text-white">
+                            <td style="border-radius: 20px; border: none; display: block; width: 100%;" class="bg-success mb-2">
+                                <i class="fa fa-list"></i>&nbsp;
+                                <strong>{{ item.nome }}</strong>
+                            </td><br>
+                        </Link>
                     </tr>
                 </tbody>
-
             </table>
         </div>
-        <br>
-        <div class="col-sm-12">
 
-            <Link @click="confirmFormAdd"
-                class="btn btn-info snRegular btn-block" style="border-radius: 20px;">
-                CRIE UMA NOVA LISTAGEM
-            </Link>
-
-        </div>
-
-        <Modal :show="confirmingFormAdd" @close="closeModal">
-
-                <div class="row">
-                    <div class="col-sm-12">
-                        <h3 class="card-title">Faça aqui uma nova listagem para suas compras</h3>
-                    </div>
-                    <br><br>
-
-                    <div class="col-sm-12">
-                        <label for="name" class="form-label">Nome da lista</label>
-                        <TextInput
-                            id="name"
-                            ref="nomeInput"
-                            type="text"
-                            v-model="form.name"
-                            required
-                            autofocus
-                            autocomplete="name"
-                            @keyup.enter="cadastraLista"
-                        />
-
-                        <InputError :message="form.errors.password" class="mt-2" />
-
-                    </div>
-
+        <Modal :show="exibirModal">
+            <div class="row mt-2">
+                <div class="col-12">
+                    <label class="form-label snRegular">Nome da lista</label>
+                    <TextInput
+                        id="nome"
+                        ref="nomeInput"
+                        type="text"
+                        v-model="form.nome"
+                        class="form-control"
+                        placeholder="Escreva um nome para sua listagem"
+                    />
                 </div>
-                <br>
-                <div class="row">
-                    <div class="col-6">
-                        <Link class="btn btn-default" @click="closeModal"> Cancelar </Link>
-                    </div>
-                    <div class="col-6 text-right">
-                        <Link
-                            class="ms-3 btn btn-success"
-                            :class="{ 'd-none': form.processing }"
-                            :disabled="form.processing"
-                            @click="cadastraLista"
-                            >
-                            Criar Lista
-                        </Link>
-                    </div>
+            </div>
+
+            <div class="row mt-3">
+                <div class="col-6">
+                    <button type="button" class="btn btn-danger snRegular" @click="closeModal"> Cancelar </button>
                 </div>
+                <div class="col-6 text-right">
+                    <button @click="cadastrarLista" class="btn btn-success snRegular">
+                        Cadastrar Lista
+                    </button>
+                </div>
+            </div>
         </Modal>
-
     </AuthenticatedLayout>
 </template>

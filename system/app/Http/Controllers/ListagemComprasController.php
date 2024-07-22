f7936d45822f2b4634c48de96f23abf350b9fa51
Miriã Bastos <142639375+MiriaBastos@users.noreply.github.com>
2024-07-21 22:26:40 -0300

    Troca redirect por redirect do inertia

--- system/app/Http/Controllers/ListagemComprasController.php
+++ system/app/Http/Controllers/ListagemComprasController.php
@@ -13,15 +13,9 @@ class ListagemComprasController extends Controller
 {
     public function index($lista_id = 0)
     {
-        $lista = new Lista();
-        if($lista_id > 0) {
-            $lista = Lista::find($lista_id);
-        }
-
         $listaCompras = Lista::getlistaComprasPorUser(Auth::user()->id);
 
         $vars = [
-            'lista' => $lista,
             'listaCompras' => $listaCompras
         ];
 
@@ -39,22 +33,6 @@ class ListagemComprasController extends Controller
         return Inertia::render('Master/Listagem/lista-compras', $vars);
     }
 
-    public function postForm(Request $request)
-    {
-
-        dd($request->all());
-        // $request->validate([
-        //     'nome' => 'required',
-        // ]);
-
-        // $lista = new Lista();
-        // $lista->nome = $request->nome;
-        // $lista->save();
-
-        return 'salvou';
-        // return Redirect::route('listagem-compras.index');
-    }
-
     public function cadastrar(Request $request)
     {
         $request->validate([
@@ -66,9 +44,9 @@ class ListagemComprasController extends Controller
         $lista->nome = $request->nome;
 
         if($lista->save()) {
-            return Redirect::route('listagem.index', ['lista_id' => $lista->id]);
+            return to_route('listagem.index');
         }
 
-        return Redirect::route('listagem.index');
+        return to_route('istagem.index');
     }
 }
